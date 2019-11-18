6e6b797e44ead7cd6b6cac7da4374ec5f7bf5109
Chad Thomas <32079148+chadthom7@users.noreply.github.com>
2019-11-17 21:24:50 -0500

    Add files via upload

---
+++ bin2dec.asm
@@ -0,0 +1,94 @@
+	.text
+#-------------------------------------------------------------------------------------------
+	li 	$2,429496730	#regs(2) <= 0.1
+	li	$3,10		#regs(3) <= 10
+	sra	$4, $4, 0	#user input (replace with sra)
+	
+	multu	$4,$2			#have to use mult unsigned, bc if > .5, most sig decimal bit is 1	
+					#  which can be read as decimal bit  
+	mfhi	$4		#regs(4) <= whole part of product
+	mflo	$5		#regs(5) <= fractional part of product
+	multu	$5,$3
+	mfhi	$5		#regs(5 <= modulo value
+	sll	$5,$5,28		
+	srl	$6,$6,4
+	or	$6,$6,$5
+
+	# copy 7 more times
+#-------------------------------------------------------------------------------------------
+	multu	$4,$2			#have to use mult unsigned, bc if > .5, most sig decimal bit is 1	
+					#  which can be read as decimal bit
+					#insert srl here to output register 6 to hexdisplays
+	mfhi	$4		#regs(4) <= whole part of product
+	mflo	$5		#regs(5) <= fractional part of product
+	multu	$5,$3
+	mfhi	$5		#regs(5 <= modulo value
+	sll	$5,$5,28		
+	srl	$6,$6,4
+	or	$6,$6,$5
+#-------------------------------------------------------------------------------------------
+	multu	$4,$2			#have to use mult unsigned, bc if > .5, most sig decimal bit is 1	
+					#  which can be read as decimal bit
+					#insert srl here to output register 6 to hexdisplays
+	mfhi	$4		#regs(4) <= whole part of product
+	mflo	$5		#regs(5) <= fractional part of product
+	multu	$5,$3
+	mfhi	$5		#regs(5 <= modulo value
+	sll	$5,$5,28		
+	srl	$6,$6,4
+	or	$6,$6,$5
+#-------------------------------------------------------------------------------------------
+	multu	$4,$2			#have to use mult unsigned, bc if > .5, most sig decimal bit is 1	
+					#  which can be read as decimal bit
+					#insert srl here to output register 6 to hexdisplays
+	mfhi	$4		#regs(4) <= whole part of product
+	mflo	$5		#regs(5) <= fractional part of product
+	multu	$5,$3
+	mfhi	$5		#regs(5 <= modulo value
+	sll	$5,$5,28		
+	srl	$6,$6,4
+	or	$6,$6,$5
+#-------------------------------------------------------------------------------------------
+	multu	$4,$2			#have to use mult unsigned, bc if > .5, most sig decimal bit is 1	
+					#  which can be read as decimal bit
+					#insert srl here to output register 6 to hexdisplays
+	mfhi	$4		#regs(4) <= whole part of product
+	mflo	$5		#regs(5) <= fractional part of product
+	multu	$5,$3
+	mfhi	$5		#regs(5 <= modulo value
+	sll	$5,$5,28		
+	srl	$6,$6,4
+	or	$6,$6,$5
+#-------------------------------------------------------------------------------------------
+	multu	$4,$2			#have to use mult unsigned, bc if > .5, most sig decimal bit is 1	
+					#  which can be read as decimal bit
+					#insert srl here to output register 6 to hexdisplays
+	mfhi	$4		#regs(4) <= whole part of product
+	mflo	$5		#regs(5) <= fractional part of product
+	multu	$5,$3
+	mfhi	$5		#regs(5 <= modulo value
+	sll	$5,$5,28		
+	srl	$6,$6,4
+	or	$6,$6,$5
+#-------------------------------------------------------------------------------------------
+	multu	$4,$2			#have to use mult unsigned, bc if > .5, most sig decimal bit is 1	
+					#  which can be read as decimal bit
+					#insert srl here to output register 6 to hexdisplays
+	mfhi	$4		#regs(4) <= whole part of product
+	mflo	$5		#regs(5) <= fractional part of product
+	multu	$5,$3
+	mfhi	$5		#regs(5 <= modulo value
+	sll	$5,$5,28		
+	srl	$6,$6,4
+	or	$6,$6,$5
+#-------------------------------------------------------------------------------------------
+	multu	$4,$2			#have to use mult unsigned, bc if > .5, most sig decimal bit is 1	
+					#  which can be read as decimal bit
+					#insert srl here to output register 6 to hexdisplays
+	mfhi	$4		#regs(4) <= whole part of product
+	mflo	$5		#regs(5) <= fractional part of product
+	multu	$5,$3
+	mfhi	$5		#regs(5 <= modulo value
+	sll	$5,$5,28		
+	srl	$6,$6,4
+	or	$6,$6,$5
